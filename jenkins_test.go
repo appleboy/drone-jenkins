53beac6448dbc9c5133a6d05e7a70877aab83bfc
geopossachs <10324450+geopossachs@users.noreply.github.com>
2021-09-27 22:10:43 +0200

    change test and update ci file

--- jenkins_test.go
+++ jenkins_test.go
@@ -22,6 +22,19 @@ func TestParseJobPath(t *testing.T) {
 	assert.Equal(t, "/job/foo/job/bar", jenkins.parseJobPath("foo///bar"))
 }
 
+func TestBuildURL(t *testing.T) {
+	auth := &Auth{
+		Username: "appleboy",
+		Token:    "1234",
+	}
+	jenkins := NewJenkins(auth, "http://example.com", nil)
+
+	assert.Equal(t, "http://example.com/foo/", jenkins.buildURL("/foo/", nil))
+
+	query := url.Values{"ba": []string{"ta"}}
+	assert.Equal(t, "http://example.com/foo/?ba=ta", jenkins.buildURL("foo/", query))
+}
+
 func TestUnSupportProtocol(t *testing.T) {
 	auth := &Auth{
 		Username: "admin",
@@ -33,38 +46,10 @@ func TestUnSupportProtocol(t *testing.T) {
 	assert.NotNil(t, err)
 }
 
-func TestTriggerBuild(t *testing.T) {
-	auth := &Auth{
-		Username: "admin",
-		Token:    "116ba1f8950c5ddff3ab26d6f5acbc3e41",
-	}
-
-	var m map[string]string
-	m = make(map[string]string)
-	m["state"] = "du da"
-	m["state_value"] = "noch was"
-
-	jenkins := NewJenkins(auth, "http://jenkins:8080", m)
-
-	err := jenkins.trigger("with-param", url.Values{"token": []string{"117caafd2840748c41157c445762d07624"}})
-	assert.Nil(t, err)
-}
-
-func TestTriggerBuild2(t *testing.T) {
-	auth := &Auth{
-		Username: "admin",
-		Token:    "116ba1f8950c5ddff3ab26d6f5acbc3e41",
-	}
-	jenkins := NewJenkins(auth, "http://jenkins:8080", nil)
-
-	err := jenkins.trigger("MyFolder/drone-jenkins", url.Values{"token": []string{"117caafd2840748c41157c445762d07624"}})
-	assert.Nil(t, err)
-}
-
 func TestLoadXSRFToken(t *testing.T) {
 	auth := &Auth{
 		Username: "admin",
-		Token:    "116ba1f8950c5ddff3ab26d6f5acbc3e41",
+		Token:    "adminadmin",
 	}
 	jenkins := NewJenkins(auth, "http://jenkins:8080", nil)
 
@@ -76,5 +61,33 @@ func TestLoadXSRFToken(t *testing.T) {
 	}
 	fmt.Printf("info - load jenkinsCrumb: %+v \n", jenkinsCrumb)
 
+	assert.Nil(t, err)
+}
+
+func TestTriggerBuild1(t *testing.T) {
+	auth := &Auth{
+		Username: "admin",
+		Token:    "adminadmin",
+	}
+	jenkins := NewJenkins(auth, "http://jenkins:8080", nil)
+
+	err := jenkins.trigger("first-pipeline", url.Values{"token": []string{"117caafd2840748c41157c445762d07624"}})
+	assert.Nil(t, err)
+}
+
+func TestTriggerBuild2(t *testing.T) {
+	auth := &Auth{
+		Username: "admin",
+		Token:    "adminadmin",
+	}
+
+	//var m map[string]string
+	var m = make(map[string]string)
+	m["sValue"] = "du da"
+	m["sValue2"] = "noch was"
+
+	jenkins := NewJenkins(auth, "http://jenkins:8080", m)
+
+	err := jenkins.trigger("another-pipeline", url.Values{"token": []string{"117caafd2840748c41157c445762d07624"}})
 	assert.Nil(t, err)
 }
